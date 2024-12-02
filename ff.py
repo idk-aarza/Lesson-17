c87608e4861669e5b93a4b0c808235ba89df1a34
idk-aarza <aarzak2012@gmail.com>
2024-12-02 18:43:25 +0530

    vv

---
+++ ff.py
@@ -0,0 +1,13 @@
+for x in range(10):
+    if x % 20==0:
+        print("fizz")
+    elif x% 15==0:
+        print("twist")
+    elif x% 10==0:
+        print("buzz")
+    elif x%5==0:
+        print("twinkle")
+    elif x%3==0:
+        print("splash")
+    else:
+        print("nothing")
